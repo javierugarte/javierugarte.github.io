d15b02c35da90d0d768afb14d71daebd289c5204
Javier González <javierugarte91@gmail.com>
2016-06-09 14:21:28 +0200

    Added eldiario and bigbrother apps but hidden

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ app.controller("appCtrl", function($scope, $http) {
     $http.get($URL)
     .success(function(data) {
       
-      json = angular.fromJson(data);
+		json = angular.fromJson(data);
+
+		var bitbanApps = json["business"][0]["apps"];
+		
+      	var defApps = getApplicationWithoutHidden(bitbanApps);
+
+		$scope.bitban = defApps;
       
-      $scope.bitban = json["business"][0]["apps"];
 
     });
  
 });
+
+function getApplicationWithoutHidden(apps) {
+	var defApps = []; 
+		
+		for (var i in apps) {
+			
+			var app = apps[i];
+			
+			if (app["hidden"] == true) {
+				continue;
+			}
+			
+			defApps.push(app);	
+		}
+
+		return defApps;
+}

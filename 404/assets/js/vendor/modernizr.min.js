abe6d629c750ec385da9b4f1ab1eea31da4efa07
Javier Gonzalez <javierugarte91@gmail.com>
2020-03-07 13:13:29 +0100

    Include 404 error

---
+++ 404/assets/js/vendor/modernizr.min.js
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang-es>
+
+<!--[if lt IE 9]><html class="no-js ie lt-ie9 ie8 url-404 url-404" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js url-404 url-404" lang="en"><!--<![endif]-->
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <meta name="description" content="404 - jugarte.es">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7329872/724742/css/fonts.css" />
+    <link href="/404/assets/css/app.min.css" rel="stylesheet" type="text/css" media="all" />
+    <noscript><link href="/404/assets/css/png.min.css" rel="stylesheet" type="text/css" media="all" id="svg-fallback" /></noscript>
+    <script src="/404/assets/js/vendor/modernizr.min.js"></script>
+    <title>404 : Javier González Martín</title>
+  </head>
+  <body>
+    <div class="wrapper" id="js-wrapper">
+      <header class="page-head " id="js-page-head">
+  <div class="container">
+    <ul class="nav page-head__nav" id="js-menu-nav">
+    </ul>
+    <a href="#" class="page-head__menu" id="js-menu">Menu</a>
+  </div>
+</header>
+      <div class="container " id="js-content-block">
+
+<div class="not-found">
+  <img src="/404/assets/img/notfound.gif" alt="Space invaders">
+  <h1>Sorry, this page doesn't exist.</h1>
+  <a href="http://jugarte.es">View my web</a>
+</div>
+
+      </div>
+      <section class="portfolio-item__text js-text" id="view-more">
+      </section>
+      <footer class="page-foot">
+  <div class="container">
+
+
+    <ul class="nav page-foot__nav" id="js-footer-nav">
+    </ul>
+</div>
+  </div>
+</footer>
+      <div id="js-overlay">
+      </div>
+    <div id="js-contact-overlay"><div class="portfolio-item contact-page">
+  <section class="portfolio-item__header js-img">
+
+  </section>
+</div></div>
+    <script src="/404/assets/js/scripts.min.js"></script>
+  </body>
+</html>
